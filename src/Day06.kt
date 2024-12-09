23206e0b8ea5ba21ffed577848a0c4953a98e153
Maksymilian Splawski <maksymilian.splawski@allegro.com>
2024-12-09 17:40:49 +0100

    feat: day06, part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -64,15 +64,63 @@ fun main() {
         return distinctPositions.size + 1
     }
 
-//    fun part2(input: List<String>): Int {
-//
-//    }
+    fun part2(input: List<String>): Int {
+        val width = input[0].length
+        val height = input.size
+        val obstacles = mutableSetOf<Pair<Int, Int>>()
+        var initialPosition = 0 to 0
 
-    val input = readInput("Day06")
+        val directions = listOf(
+            0 to -1, // top
+            1 to 0, // right
+            0 to 1, // bottom
+            -1 to 0, // left
+        )
+
+        input.forEachIndexed { y, line ->
+            line.forEachIndexed { x, char ->
+                if (char == '#') obstacles.add(x to y)
+                if (char == '^') initialPosition = x to y
+            }
+        }
+
+        var looped = 0
+
+        for (outerX in 0..<width) {
+            for (outerY in 0..<height) {
+                val newObstacles = obstacles + (outerX to outerY)
+
+                var direction = directions.first()
+                var position = initialPosition
+
+                fun turnRight() {
+                    val i = (directions.indexOf(direction) + 1) % directions.size
+                    direction = directions[i]
+                }
+
+                fun nextPosition() = (position.first + direction.first) to (position.second + direction.second)
+
+                val visited = mutableSetOf<Pair<Pair<Int, Int>, Pair<Int, Int>>>()
+                while (position.first in 0..width && position.second in 0..height) {
+                    if (position to direction in visited) {
+                        looped++
+                        break
+                    }
+                    visited += (position to direction)
+                    val nextPosition = nextPosition()
+                    if (nextPosition in newObstacles) {
+                        turnRight()
+                    } else {
+                        position = nextPosition
+                    }
+                }
+            }
+        }
+
+        return looped
+    }
+
+    val input = readInput("Day06_test")
     part1(input).println()
-//    part2(input).println()
-}
-
-private operator fun String.set(i: Int, value: Char) {
-    this[i] = value
+    part2(input).println()
 }
